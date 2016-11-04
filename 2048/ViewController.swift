bfcbc25113055a9b0fc33f2963746f8122b2e243
Omar Griffin <treylitefm@Omars-MacBook-Air.local>
2016-11-04 10:27:58 -0700

    Initial Commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Omar Griffin on 11/4/16.
+//  Copyright © 2016 Omar Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
